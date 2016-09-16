d24f77072019fd120f251b0b4137d3275acafc37
vunnikri <zinvith@gmail.com>
2016-09-17 00:10:39 +0530

    Create console.js

---
+++ app/consoles/console.js
@@ -0,0 +1,35 @@
+angular.module('uiRouterSample.home', [
+  'ui.router'
+])
+  
+.config(
+  [          '$stateProvider', '$urlRouterProvider',
+    function ($stateProvider,   $urlRouterProvider) {
+    
+      $stateProvider
+      
+        /////////////////
+        // Home Screen //
+        /////////////////
+
+        .state('home', {
+          url: '/home',
+          views: {
+
+            '': {
+              templateUrl: 'app/home/home.html',
+              controller: ['$scope', '$state',
+                function (  $scope,   $state) {
+                  alert("in home controller");
+                }]
+            },
+
+            'navigation-bar@': {
+              templateUrl: 'app/navbar/console-navbar.html'
+            }
+          }
+        })
+        ;
+    }
+  ]
+);
