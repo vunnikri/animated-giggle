7bf75b223bed0d0f494d5f5fc601ddc34ba53b1c
vunnikri <zinvith@gmail.com>
2016-09-17 01:05:25 +0530

    Update console.js

--- app/console/console.js
+++ app/console/console.js
@@ -1,1 +1,35 @@
+angular.module('uiRouterSample.console', [
+  'ui.router'
+])
+  
+.config(
+  [          '$stateProvider', '$urlRouterProvider',
+    function ($stateProvider,   $urlRouterProvider) {
+    
+      $stateProvider
+      
+        /////////////////
+        // Home Screen //
+        /////////////////
 
+        .state('console', {
+          url: '/console',
+          views: {
+
+            '': {
+              templateUrl: 'app/console/console.html',
+              controller: ['$scope', '$state',
+                function (  $scope,   $state) {
+                  alert("in console controller");
+                }]
+            },
+
+            'navigation-bar@': {
+              templateUrl: 'app/navbar/console-navbar.html'
+            }
+          }
+        })
+        ;
+    }
+  ]
+);
