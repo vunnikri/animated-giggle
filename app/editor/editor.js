1bc96ea8e2b28d489d9b51acc0d87ad60c8c5c2f
vunnikri <zinvith@gmail.com>
2016-09-17 01:14:37 +0530

    Create editor.js

---
+++ app/editor/editor.js
@@ -0,0 +1,35 @@
+angular.module('uiRouterSample.editor', [
+  'ui.router'
+])
+  
+.config(
+  [          '$stateProvider', '$urlRouterProvider',
+    function ($stateProvider,   $urlRouterProvider) {
+    
+      $stateProvider
+      
+        /////////////////
+        // Home Screen //
+        /////////////////
+
+        .state('editor', {
+          url: '/editor',
+          views: {
+
+            '': {
+              templateUrl: 'app/editor/editor.html',
+              controller: ['$scope', '$state',
+                function (  $scope,   $state) {
+                  alert("in editor controller");
+                }]
+            },
+
+            'navigation-bar@': {
+              templateUrl: 'app/navbar/console-navbar.html'
+            }
+          }
+        })
+        ;
+    }
+  ]
+);
