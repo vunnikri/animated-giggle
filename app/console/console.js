ad77f77c4b4b2b5aee116b3b32fd01131ce1ac13
vunnikri <zinvith@gmail.com>
2016-09-19 22:43:25 +0530

    Update console.js

--- app/console/console.js
+++ app/console/console.js
@@ -38,8 +38,59 @@ angular.module('uiRouterSample.console', [
                     { name: 'Gender', field: 'gender', width: "10%" },
                     { name: 'Dependants', field: 'dependant', width: "10%" },
                     
-                    
-                    ]
+                    { name: 'Entry Offer', field: 'offer', width: "13%", enableFiltering: false,
+                      cellClass: function(grid, row, col, rowRenderIndex, colRenderIndex)
+                      {
+                        if(row.entity.offerStatus === 'OK') { return 'status-ok'; }
+                        else if(row.entity.offerStatus === 'Inactive')  { return 'status-inactive'; }
+                        else if(row.entity.offerStatus === 'Rejected')  { return 'status-error'; }
+                      }
+                    },
+                    { name: 'Resident Country', field: 'country', width: "13%" },
+                    { name: 'English Score', field: 'englishScore', width: "13%", enableFiltering: false,
+                      cellClass: function(grid, row, col, rowRenderIndex, colRenderIndex)
+                      {
+                        if(row.entity.englishScoreStatus === 'HIGH') { return 'status-ok'; }
+                        else if(row.entity.englishScoreStatus === 'MEDIUM')  { return 'status-inactive'; }
+                        else if(row.entity.englishScoreStatus === 'LOW')  { return 'status-error'; }
+                      }
+                    },
+                    { name: 'Days Remaining', field: 'daysRemaining', width: "12%", cellClass: "console-text-center" },
+                    { name: 'Status', field: 'applicantStatus', width: "11%", enableFiltering: false,
+                      cellClass: function(grid, row, col, rowRenderIndex, colRenderIndex)
+                      {
+                        if(row.entity.applicantStatus === 'APPROVED') { return 'status-ok'; }
+                        else if(row.entity.applicantStatus === 'ON-HOLD')  { return 'status-hold'; }
+                        else if(row.entity.applicantStatus === 'REJECTED')  { return 'status-error'; }
+                      }
+                    }
+                  ];
+                  
+                  $scope.consolegrid.multiSelect = false;
+                  $scope.consolegrid.modifierKeysToMultiSelect = false;
+                  $scope.consolegrid.noUnselect = false;
+                  
+                  $scope.consolegrid.onRegisterApi = function ( gridApi) {
+                    $scope.gridApi = gridApi;
+                    gridApi.selection.on.rowSelectionChanged($scope, function(row){
+                      if(row.isSelected)
+                      {
+                        if(row.entity && row.entity.name)
+                        {
+                          //Save the selected application into singleton
+                          //singleton.setConsoleRow(row);
+                          $rootScope.enableConfiguration = "enabled";
+                        }
+                      }
+                    });
+                  };
+                  
+                  $scope.toggleRowSelection = function() {
+                    $scope.gridApi.selection.clearSelectedRows();
+                    $scope.consolegrid.enableRowSelection = !$scope.consolegrid.enableRowSelection;
+                    $scope.gridApi.core.notifyDataChange( uiGridConstants.dataChange.OPTIONS);
+                  };
+                  
                   
                 }]
             },
